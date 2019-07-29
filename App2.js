fe63b2fd14dd2c3fcd2e9e671347d5b7433e14f5
root <evansmwenda.em@gmail.com>
2019-07-29 17:19:08 +0300

    last update monday

--- App2.js
+++ App2.js
@@ -1,55 +1,56 @@
 import React, { Component } from 'react';
-import { Text, Image , FlatList, ScrollView , Alert, Button, TextInput, View , StyleSheet, TouchableHighlight,TouchableNativeFeedback,TouchableOpacity,TouchableWithoutFeedback} from 'react-native';
+import { Text, Platform, Image ,ActivityIndicator,  FlatList, ScrollView , Alert, Button, TextInput, View , StyleSheet, TouchableHighlight,TouchableNativeFeedback,TouchableOpacity,TouchableWithoutFeedback} from 'react-native';
 
 export default class PizzaTranslator extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state ={ isLoading: true}
   }
 
-  _onPressButton(){
-  	Alert.alert("you tapped button");
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
   }
 
-  _onLongPressButton(){
-  	Alert.alert("you long pressed button");
-  }
 
-  render() {
-    return (
-    	 <View style={styles.container}>
-    	 	<FlatList 
-    	 	   data={[
-    	 	   	{key:"Kevin"},
-    	 	   	{key:"Mitnick"},
-    	 	   	{key:"Mike"},
-    	 	   	{key:"Tyson"},
-    	 	   	{key:"Eliud"},
-    	 	   	{key:"Kipchoge"},
-    	 	   	{key:"Scarlet"},
-    	 	   	{key:"Reddington"},
-    	 	   	{key:"Bowvinsky"},
-    	 	   	{key:"Lockheed"},
-    	 	   	{key:"Moses"},
-    	 	   	{key:"Adam"},
-    	 	   	{key:"Noah"},
-    	 	   	{key:"Enoch"},
-    	 	   	{key:"Martha"},
-    	 	   	{key:"John"},
-    	 	   	{key:"Jacinta"},
-    	 	   	{key:"Janet"},
-    	 	   	{key:"Oneal"},
-    	 	   	{key:"Joshua"},
-    	 	   	{key:"Japheth"}
-    	 	   	]} 
-    	 	   renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>} 
-    	 	   />
+  render(){
 
-    	 </View>
-    	
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
 
+    return(
+    <ScrollView>
+    <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear} {Platform.OS} -> {Platform.Version}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+        <Image source={{uri: 'https://facebook.github.io/react/logo-og.png'}}
+       style={{width: 400, height: 400}} />
+      </View>
+      </ScrollView>
+      
     );
-}
+  }
  
 }
 
